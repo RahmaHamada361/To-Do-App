cd1ffb404cc1be24b3af53194f9d84642356b7c1
Rahma Hamada <rahmahamada361@gmail.com>
2024-01-18 21:34:57 +0100

    To Do App

---
+++ main.js
@@ -0,0 +1,18 @@
+let addButton = document.getElementById('add');
+let toDoContainer = document.getElementById('toDoContainer');
+let input = document.getElementById('input');
+
+
+addButton.addEventListener('click', function(){
+    var paragraph = document.createElement('p')
+    paragraph.classList.add('paragraph-styling');
+    paragraph.innerText = input.value;
+    toDoContainer.appendChild(paragraph);
+    input.value ="";
+    paragraph.addEventListener('click', function(){
+        paragraph.style.textDecoration = "line-through";
+    })
+    paragraph.addEventListener('dblclick', function(){
+        toDoContainer.removeChild(paragraph);
+    })
+})
